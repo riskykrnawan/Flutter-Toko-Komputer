59e5206fe964a08dd5475286b5184607b72ce143
Xavier <gemparpanggih123@gmail.com>
2022-06-17 20:14:33 +0800

    Profile Page
    agak ribet ki. aku sistem nya ngambil template dari orang lain

---
+++ lib/screens/profile/pages/edit_description.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:acul_komputer/screens/profile/user/user_data.dart';
+import 'package:acul_komputer/screens/profile/widgets/appbar_widget.dart';
+
+class EditDescriptionFormPage extends StatefulWidget {
+  @override
+  _EditDescriptionFormPageState createState() =>
+      _EditDescriptionFormPageState();
+}
+
+class _EditDescriptionFormPageState extends State<EditDescriptionFormPage> {
+  final _formKey = GlobalKey<FormState>();
+  final descriptionController = TextEditingController();
+  var user = UserData.myUser;
+
+  @override
+  void dispose() {
+    descriptionController.dispose();
+    super.dispose();
+  }
+
+  void updateUserValue(String description) {
+    user.aboutMeDescription = description;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: buildAppBar(context),
+        body: Form(
+          key: _formKey,
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(
+                  width: 350,
+                  height: 29,
+                  child: const Text(
+                    "Input Deskripsi",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: SizedBox(
+                    width: 350,
+                    height: 193,
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            value.length > 200) {
+                          return 'Kolom Kosong';
+                        }
+                        return null;
+                      },
+                      controller: descriptionController,
+                      textAlignVertical: TextAlignVertical.top,
+                      decoration: const InputDecoration(
+                          alignLabelWithHint: true,
+                          contentPadding: EdgeInsets.fromLTRB(10, 15, 10, 100),
+                          hintMaxLines: 3,
+                          hintText:
+                              'Nothing Last Forever We Can Change the Future.'),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 0),
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: SizedBox(
+                      width: 350,
+                      height: 50,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            updateUserValue(descriptionController.text);
+                            Navigator.pop(context);
+                          }
+                        },
+                        child: const Text(
+                          'Update',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ]),
+        ));
+  }
+}
---
+++ lib/screens/profile/pages/profile_page.dart
@@ -0,0 +1,165 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'edit_description.dart';
+import 'edit_email.dart';
+import 'edit_image.dart';
+import 'edit_name.dart';
+import 'edit_phone.dart';
+import '../user/user.dart';
+import '../widgets/display_image_widget.dart';
+import '../user/user_data.dart';
+
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    final user = UserData.myUser;
+
+    return Scaffold(
+      body: Column(
+        children: [
+          AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            toolbarHeight: 10,
+          ),
+          SizedBox(height: 16),
+          Center(
+              child: Padding(
+                  padding: EdgeInsets.only(bottom: 20),
+                  child: Text(
+                    'Profile',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w700,
+                      color: Color.fromRGBO(64, 105, 225, 1),
+                    ),
+                  ))),
+          InkWell(
+              onTap: () {
+                navigateSecondPage(EditImagePage());
+              },
+              child: DisplayImage(
+                imagePath: user.image,
+                onPressed: () {},
+              )),
+          buildUserInfoDisplay(user.name, 'Nama', EditNameFormPage()),
+          buildUserInfoDisplay(user.phone, 'Phone', EditPhoneFormPage()),
+          buildUserInfoDisplay(user.email, 'Email', EditEmailFormPage()),
+          Expanded(
+            child: buildAbout(user),
+            flex: 4,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildUserInfoDisplay(String getValue, String title, Widget editPage) =>
+      Padding(
+          padding: EdgeInsets.only(bottom: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey,
+                ),
+              ),
+              SizedBox(
+                height: 1,
+              ),
+              Container(
+                  width: 350,
+                  height: 40,
+                  decoration: BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(
+                    color: Colors.grey,
+                    width: 1,
+                  ))),
+                  child: Row(children: [
+                    Expanded(
+                        child: TextButton(
+                            onPressed: () {
+                              navigateSecondPage(editPage);
+                            },
+                            child: Text(
+                              getValue,
+                              style: TextStyle(fontSize: 16, height: 1.4),
+                            ))),
+                    Icon(
+                      Icons.keyboard_arrow_right,
+                      color: Colors.grey,
+                      size: 40.0,
+                    )
+                  ]))
+            ],
+          ));
+
+  Widget buildAbout(User user) => Padding(
+      padding: EdgeInsets.only(bottom: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Deskripsi Profile',
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+              color: Colors.grey,
+            ),
+          ),
+          const SizedBox(height: 1),
+          Container(
+              width: 350,
+              height: 200,
+              decoration: BoxDecoration(
+                  border: Border(
+                      bottom: BorderSide(
+                color: Colors.grey,
+                width: 1,
+              ))),
+              child: Row(children: [
+                Expanded(
+                    child: TextButton(
+                        onPressed: () {
+                          navigateSecondPage(EditDescriptionFormPage());
+                        },
+                        child: Padding(
+                            padding: EdgeInsets.fromLTRB(0, 10, 10, 10),
+                            child: Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  user.aboutMeDescription,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    height: 1.4,
+                                  ),
+                                ))))),
+                Icon(
+                  Icons.keyboard_arrow_right,
+                  color: Colors.grey,
+                  size: 40.0,
+                )
+              ]))
+        ],
+      ));
+
+  FutureOr onGoBack(dynamic value) {
+    setState(() {});
+  }
+
+  void navigateSecondPage(Widget editForm) {
+    Route route = MaterialPageRoute(builder: (context) => editForm);
+    Navigator.push(context, route).then(onGoBack);
+  }
+}
